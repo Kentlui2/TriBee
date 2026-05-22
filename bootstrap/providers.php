b69ebe1715ad4ec068b8ac3dce881bd5a45e2d6d
nano-saku <j.m.mschoolmail@gmail.com>
2026-05-23 04:11:20 +0800

    G3: Merge main with G2 products into G3 branch

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,8 +2,10 @@
 
 use App\Providers\AppServiceProvider;
 use App\Modules\Cart\CartServiceProvider;
+use App\Modules\Products\Providers\ProductServiceProvider;
 
 return [
     AppServiceProvider::class,
     CartServiceProvider::class,
+    ProductServiceProvider::class,
 ];
